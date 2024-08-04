cdb6dd3ea196d62da763f701c2172af18add16a6
callumJPritchard <cjohnpritchard@gmail.com>
2024-08-04 22:09:51 +0100

    slowgress

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,28 @@
 import { Funcs } from "./types"
 
-const { html, body, div, h1, h2, p } = Funcs
+const { html, body, div, h1, h2, p, button } = Funcs
+
+function ComponentExample() {
+    return div(
+        h1("Hello, component world!"),
+        p("This is a component paragraph."),
+        button("Click me!")
+    )
+}
 
 const app =
     html(
         body(
             div(
+                {id: "something"},
                 h1("Hello, world!"),
                 p("This is a paragraph.")
             ),
             div(
                 h2("Another div."),
                 p("This is another paragraph.")
-            )
+            ),
+            ComponentExample()
         )
     )
 
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,12 +1,34 @@
 import { HtmlTags } from "./tags"
 
-export type FuncsType = Record<HtmlTags, (...args: any[]) => string>
+type ArgType = string | Record<string, string> | undefined
+type ComponentType = (...args: ArgType[]) => string
+
+export type FuncsType = Record<HtmlTags, ComponentType>
 
 export const Funcs = new Proxy({} as FuncsType, {
     get(target, prop) {
-        const p = String(prop)
-        return (...args: any[]) => {
-            return `<${p}>${args.join("")}</${p}>`
+        const ret: ComponentType = (...args: ArgType[]) => {
+
+            let attributes = ""
+
+            for (const arg of args) {
+                if (typeof arg === "object" && !Array.isArray(arg)) {
+                    for (const key in arg) {
+                        attributes += ` ${key}="${arg[key]}"`; // Using += to accumulate the attributes string
+                    }
+                }
+            }
+
+
+            const start = `<${prop as string}${attributes}>`
+
+            if (args.length === 0) {
+                return `${start}</${prop as string}>`
+            }
+
+
+            return `${start}${args.join("")}</${prop as string}>`
         }
+        return ret
     }
 })
