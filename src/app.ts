2892552bf2ffd8050c2d3d4770bbe06a29656133
Callum Pritchard <cjohnpritchard@gmail.com>
2024-08-07 16:03:49 +0100

    init

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,47 @@
-import FFF from "./index";
+import { HtmlTags } from "./types/tags";
 
-const { tags, mount, useState } = FFF;
-const { div, h1, h2, p, button } = tags;
+export type ArgType = string | TreeNode;
 
-const [count, setCount] = useState(0);
-function ComponentExample() {
-  console.log("rendering component");
-  return div(
-    h1("Hello, component world!"),
-    p(count + ""),
-    button({ onclick: () => setCount(count + 1) }, "Click me!")
-  );
+export type TagType = (...args: ArgType[]) => TreeNode;
+
+export interface TreeNode {
+  tag: string;
+  children: ArgType[];
 }
+
+export type FuncsType = Record<HtmlTags, TagType>;
+
+function FFFProto() {
+  const tree: TreeNode[] = [];
+
+  const tags = new Proxy({} as FuncsType, {
+    get(_, prop: string) {
+      return (...args: ArgType[]) => {
+        const node: TreeNode = { tag: prop, children: args };
+        console.log(node);
+        tree.push(node);
+        return node;
+      };
+    },
+  });
+
+  return {
+    tags,
+    tree,
+  };
+}
+
+const FFF = FFFProto();
+
+const { tags } = FFF;
+const { div, h1, h2, p, button } = tags;
 
 const app = () =>
   div(
-    div({ id: "something" }, h1("Hello, world!"), p("This is a paragraph.")),
-    div(h2("Another div."), p("This is another paragraph.")),
-    ComponentExample()
+    div(h1("Hello, world!"), p("This is a paragraph.")),
+    div(h2("Another div."), p("This is another paragraph."))
   );
 
-mount(app, "app");
+const tree = app();
+
+console.log(JSON.stringify(tree, null, 2));
